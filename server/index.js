331b7896d4df438b607012cb8712b46430b89156
Raghav Agrawal <raghav170101@gmail.com>
2023-03-17 17:17:31 +0900

    🔒 added domain for cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
 const express = require('express');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.DOMAIN,
+        optionsSuccessStatus: 200,
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('Home');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server is up at ${PORT}`);
-})
+});
